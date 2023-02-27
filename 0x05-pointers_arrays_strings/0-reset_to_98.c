daabbd049b95536925b0c437a2f17ba9ac6c03bf
adelbert-samuel <s.k.adelbert@gmail.com>
2023-02-27 07:41:30 -0800

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*
+ * File:0-reset_to_98.c
+ * Auth: Adelbert Samuel
+ * reseet_to_98 - a function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ * @n: input
+ * Return:n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
